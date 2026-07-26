e1fd9d693d8e620077746e5abba0b25618036b77
Abhay Singh <abhaysinghh1884@gmail.com>
2026-07-27 05:13:35 +0530

    LeetCode Solution: Minimum Add to Make Parentheses Valid - Runtime: 0 ms (100.0%), Memory: 9.6 MB (7.0%)

---
+++ 0957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<int>st;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(st.empty() || s[i]=='(' ){
+                st.push(s[i]);
+            }
+            else if(s[i]==')' &&st.top()=='('){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        return st.size();
+    }
+};
